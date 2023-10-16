fc06f677f9cb87e805fc023481582242b687b925
CesarABaron <127266668+CesarABaron@users.noreply.github.com>
2023-10-15 22:23:53 -0500

    se creo back con node nodemon express y otra vaina aahi

---
+++ API/Server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 6000;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
